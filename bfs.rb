83a301880f7afa43a32e2cbdb1e9637ccb62b089
Nabil J. Marquez <nabiljesus@gmail.com>
2016-03-10 01:27:09 -0430

    Avances en el bfs. Problemas con el path. Evita la recursion infinita

--- bfs.rb
+++ bfs.rb
@@ -1,3 +1,93 @@
+module BFS
+        def find(start,&predicate)
+
+            queue = []
+            queue.push(start)
+            visited = []
+
+            while(!queue.empty?)
+              node = queue.shift
+              puts node.value
+              visited.push(node)
+
+              val=evalPred(node,&predicate)
+              unless val.nil?
+                return val
+              end
+
+              node.each do |child|
+                if !(visited.include? child)
+                  queue.push(child)
+                end
+              end
+            end
+
+            return nil
+        end
+
+        def path(start,&predicate)
+
+            queue = []
+            queue.push([0,start])
+            visited = []
+            ograph = Hash[]
+
+            while(!queue.empty?)
+              aux  = queue.shift
+              node = aux[1]
+              counter = aux[0]
+              #puts node.value
+              visited.push(node)
+              puts "GET IT"
+              puts node.value
+              
+              val=evalPred(node,&predicate)
+              unless val.nil?
+                puts "maybe"
+                puts node.value
+                puts counter
+                return getPath(ograph,counter,[node])
+              end
+
+              oldcounter=counter
+              node.each do |child|
+                if !(visited.include? child)
+                  counter+=1
+                  queue.push([counter,child])
+                  ograph[counter]=[oldcounter,node]
+                end
+              end
+            end
+
+            return nil
+        end
+
+        def getPath(graph,counter,path=[])
+          puts 'Entered'
+          if counter==0
+            puts 'llegue a 0'
+            return path
+          end
+          val = graph[counter]
+          if val.nil?
+            return nil
+          else
+            path.unshift(val[1])
+            puts '----'
+            puts val[1].value
+            return getPath(graph,val[0],path)
+          end
+        end
+        private :getPath 
+
+        def evalPred(node,&p)
+          if p.call(node)
+              return node
+          end   
+        end
+        private :evalPred 
+    end
+
 
 class BinTree
     attr_accessor :value, :left, :right
@@ -7,15 +97,17 @@ class BinTree
         @left = l
         @right = r
     end
-    def each() #duda preguntar si es each(b) o asi
-        yield value
+    def each(&b) #duda preguntar si es each(b) o asi
+        #[b.call(self),self,right]
         unless left.nil?
-            left.each {|leftch| yield leftch}
+            b.call(left)
         end
         unless right.nil?
-            right.each {|rightch| yield rightch}
+            b.call(right)
         end
     end
+
+    include BFS
 end
 
 
@@ -26,17 +118,17 @@ class GraphNode
         @value=v
         @children=c
     end
-    def each() #Misma duda
-        yield value
-        unless children.empty?
-            children.each do |child|
-                #yield "qlq"
-                unless child.nil?
-                    child.each {|ichild| yield ichild}
-                end
+    def each(&b) #Misma duda
+        #yield value
+        children.each do |child|
+            #yield "qlq"
+            unless child.nil?
+                b.call(child)
             end
         end
     end
+
+    include BFS
 end
 
 #Repetir bfs, utilizando mixin.
--- tbfs.rb
+++ tbfs.rb
@@ -1,25 +1,54 @@
 require_relative 'bfs'
 
-# load('rps.rb')
-# treel  = BinTree.new(11)
-# treer = BinTree.new(15)
-# tree       = BinTree.new(10,treel,treer)
-# puts tree.left.value
-# puts tree.right.value
-# puts tree.value
-# puts 'trululu'
 
-# tree.each { |value| puts "#{value}"}
+tree0  = BinTree.new(0)
+tree2  = BinTree.new(2)
+tree1  = BinTree.new(1,tree0,tree2)
+tree5  = BinTree.new(5)
+tree4  = BinTree.new(4,tree5)
+tree6  = BinTree.new(6,tree4)
+tree3  = BinTree.new(3,tree1,tree6)
+tree10  = BinTree.new(10)
+tree11  = BinTree.new(11,tree10)
+tree8  = BinTree.new(8)
+tree9  = BinTree.new(9,tree8,tree11)
+tree14  = BinTree.new(14)
+tree15  = BinTree.new(15,tree14)
+tree13  = BinTree.new(13,tree15)
+tree12  = BinTree.new(12,tree9,tree13)
+tree       = BinTree.new(7,tree3,tree12)
+#tree0.left = tree
+#treel.left=tree
+#puts tree.left.value
+#puts tree.right.value
+#puts tree.value
+puts 'trululu'
 
-puts "graph shit"
+#tree.each { |value| print "#{value.value}--"}
+#tree.left.each { |value| print "#{value.value}--"}
 
-x=[2,3,4,5]
-y=x.collect{|v| GraphNode.new(v)}
-graphe=GraphNode.new(1)
-y.push(graphe)
-y.push(GraphNode.new(10000))
-graph=GraphNode.new(1,y)
+#puts 'hia\n'
 
-graph.each { |value| print "#{value}\n"}
+x=proc {|x| x.value>13}
 
 
+y=tree.path(tree,&x)
+puts y.nil?
+puts "FUCK THIS SHIT"
+y.each {|v| puts v.value}
+puts "\ngraph shit"
+
+# x=[2,3,4,5]
+# y=x.collect{|v| GraphNode.new(v)}
+# graphe=GraphNode.new(1)
+# y.push(graphe)
+# y.push(GraphNode.new(10000))
+# graph=GraphNode.new(1,y)
+
+# w=graph.each { |value| print "#{value}\n"}
+# puts 'meeeh'
+# puts w
+
+#puts "find"
+#isodd = lambda { |i| i % 2 == 1 }
+#graph.find(graph,isodd)
